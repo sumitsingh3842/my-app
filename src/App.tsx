f6b137364c11843cf34f9b608ec345e4619bd017
Sumit Singh <sumitsingh3842@gmail.com>
2023-12-19 23:25:01 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,44 @@ import Home from './screens/Home';
 import DashBoard from './screens/DashBoard';
 import { Provider } from 'react-redux'
 import {store} from './app/store'
+import { useAuth0 } from '@auth0/auth0-react';
 import Profile from './screens/Profile';
+import Loading from './components/Loading/Loading';
 import OrganisationDetails from './screens/OrganisationDetails';
 import ChatBot from './screens/ChatBot';
 
 
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />,
-  //   },
-  //   {
-  //     path: "dashboard",
-  //     element: <DashBoard />,
-  //   },
-  //   {
-  //     path: "profile",
-  //     element: <Profile />,
-  //   },
-  //   {
-  //     path: "project/:orgName/:orgId",
-  //     element: <OrganisationDetails />,
-  //   },
-  //   {
-  //     path: "create-chat-bot",
-  //     element: <ChatBot />,
-  //   }
-  // ]);
+  const router = createBrowserRouter([
+    {
+      path: "my-app",
+      element: <Home />,
+    },
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "dashboard",
+      element: <DashBoard />,
+    },
+    {
+      path: "profile",
+      element: <Profile />,
+    },
+    {
+      path: "project/:orgName/:orgId",
+      element: <OrganisationDetails />,
+    },
+    {
+      path: "create-chat-bot",
+      element: <ChatBot />,
+    }
+  ]);
   return (
     <Provider store={store}>
-      <Home />
+    <RouterProvider router={router} />
     </Provider>
   );
 }
