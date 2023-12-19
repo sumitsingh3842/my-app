9b6825193b7f7dd50d8b3ec7d8cebe31b7fd7ad6
Sumit Singh <sumitsingh3842@gmail.com>
2023-12-19 22:34:50 +0530

    host page

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { Auth0Provider } from '@auth0/auth0-react';
-
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-    domain="dev-e8ngvuo2ygnrkkuq.us.auth0.com"
-    clientId="PNjOJHf22DLmXJ9wVAbxpNwfotMtqmVr"
-    authorizationParams={{
-      redirect_uri: "http://localhost:3000/dashboard"
-    }}
-  >
     <App />
-  </Auth0Provider>
   </React.StrictMode>
 );
