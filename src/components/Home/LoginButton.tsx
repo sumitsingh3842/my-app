d780f5ca7765506535e6c69ad02bf45ba68e89b1
Sumit Singh <sumitsingh3842@gmail.com>
2023-12-19 23:33:51 +0530

    deploy

--- src/components/Home/LoginButton.tsx
+++ src/components/Home/LoginButton.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const navigate = useNavigate();
   const loginWithRedirectFunc = async() => {
-    await loginWithRedirect();
+    navigate('/my-app/dashboard')
     
   }
   return <Button onClick={() => loginWithRedirectFunc()} color="inherit">Login</Button>
